6271b66d20edabcd3d926d3a04bf532b44d1a0e3
Piotr Zacha <pzacha11@gmail.com>
2020-05-18 13:36:52 +0200

    JS added to home page.

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 from .models import Tweet
 
 # Create your views here.
-def home_page(request, *args, **kwargs):
+def home_page_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
-def tweet_detail(request, tweet_id, *args, **kwargs):
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {"isUser": False, "response": tweets_list}
+    return JsonResponse(data)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
         "id": tweet_id,
     }
--- twitter_clone/twitter_clone/urls.py
+++ twitter_clone/twitter_clone/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from tweets.views import home_page, tweet_detail
+from tweets.views import home_page_view, tweet_detail_view, tweet_list_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home_page),
-    path("tweets/<int:tweet_id>", tweet_detail),
+    path("", home_page_view),
+    path("tweets/", tweet_list_view),
+    path("tweets/<int:tweet_id>", tweet_detail_view),
 ]
