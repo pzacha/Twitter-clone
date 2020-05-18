c80753e0f3f5adf2a83b8fb23320e62695d0c7dc
Piotr Zacha <pzacha11@gmail.com>
2020-05-18 11:52:04 +0200

    dummy template added.

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -1,16 +1,23 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
 
 # Create your views here.
 def home_page(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_detail(request, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data["message"] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
